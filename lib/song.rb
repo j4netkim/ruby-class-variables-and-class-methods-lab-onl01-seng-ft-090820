ca08443e336595873a3f178ac25e0f722e0ca0e8
J K <kj688895@gmail.com>
2020-09-21 01:40:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,11 +10,18 @@ class Song
     @artist = artist
     @genre = genre
     @@genres << @genre
-    @@artists << @artist 
+    @@artists << @artist
     @@count += 1
   end
 
   def self.count
     @@count
   end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq 
 end
